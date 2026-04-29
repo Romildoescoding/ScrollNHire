6f546a34ba6a036097d49e9b0ca30fbe6176ed13
Romildoescoding <romilrajrana1@gmail.com>
2026-04-29 14:29:34 +0530

    chore: continued landing page

--- scrollnhire/app/page.tsx
+++ scrollnhire/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import SmoothScrollProvider from "@/components/smooth_scroll_provider";
+import { AnimatedUnderline } from "@/components/ui/animated-underline";
 import { Button } from "@/components/ui/button";
-import { Moon, Sun } from "lucide-react";
+import { ChevronRight, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const LandingPage = () => {
@@ -28,27 +31,127 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="h-full w-full">
-      {/* HERO */}
-      <div className="h-svh sm:h-[50vh] bg-white w-full">
-        ScrollnHire
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={toggleTheme}
-          className="rounded-full"
-        >
-          {theme === "dark" ? (
-            <Sun className="size-[18px]" />
-          ) : (
-            <Moon className="size-[18px]" />
-          )}
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </div>
+    <SmoothScrollProvider>
+      <div className="h-full w-full">
+        {/* NAVBAR */}
+        <div className="w-full h-fit top-0 left-0 flex items-center justify-center fixed z-10 p-4">
+          <div className="w-full max-w-3xl flex items-center justify-between border p-2 rounded-2xl bg-background backdrop-blur-md shadow-sm">
+            {/*  */}
+            <div className="ml-[6px]">
+              {/* ScrollnHire */}
 
-      {/* U shape light to dark */}
-      {/* <Image
+              <Image
+                src="/logo_white.svg"
+                height={60}
+                width={60}
+                alt="gradient"
+                className="hidden dark:block h-6 w-6"
+              />
+              <Image
+                src="/logo_black.svg"
+                height={60}
+                width={60}
+                alt="gradient"
+                className="dark:hidden block h-6 w-6"
+              />
+            </div>
+
+            <div className="flex h-full items-center gap-16">
+              <div className="h-full flex gap-4 items-center">
+                <AnimatedUnderline text="FAQ" link="/" />
+                <AnimatedUnderline text="Features" link="/features" />
+                <AnimatedUnderline text="Pricing" link="/pricing" />
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={toggleTheme}
+                  className="rounded-xl"
+                >
+                  {theme === "dark" ? (
+                    <Sun className="size-[18px]" />
+                  ) : (
+                    <Moon className="size-[18px]" />
+                  )}
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+                <Link
+                  href="/login"
+                  className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Button variant="outline" className="rounded-xl text-xs py-4">
+                    Login
+                  </Button>
+                </Link>
+                <Link href="/login">
+                  <Button className="rounded-xl text-xs py-4">
+                    Get Started
+                    <ChevronRight className="ml-1 size-4" />
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* HERO */}
+        <div className="h-svh text-7xl sm:h-[50vh] bg-white dark:bg-black w-full flex flex-col items-center justify-end">
+          <div className="font-semibold flex flex-col items-center justify-end">
+            <div className="flex gap-4">
+              <div className="flex gap-2">
+                Scroll{" "}
+                <div className="h-full aspect-square border rounded-xl bg-foreground">
+                  <div className="h-full w-full flex items-center justify-center">
+                    <div className="h-12 w-12 overflow-hidden relative">
+                      {/* DARK MODE */}
+                      <Image
+                        src="/logo_black.svg"
+                        height={60}
+                        width={60}
+                        alt="gradient"
+                        className="animate-scroll1 hidden absolute top-0 left-1/2 -translate-x-1/2 dark:block h-12 w-12"
+                      />
+                      <Image
+                        src="/logo_black.svg"
+                        height={60}
+                        width={60}
+                        alt="gradient"
+                        className="animate-scroll2 absolute top-full left-1/2 -translate-x-1/2 hidden dark:block h-12 w-12"
+                      />
+                      {/* LIGHT MODE */}
+                      <Image
+                        src="/logo_white.svg"
+                        height={60}
+                        width={60}
+                        alt="gradient"
+                        className="animate-scroll1 block absolute top-0 left-1/2 -translate-x-1/2 dark:hidden h-12 w-12"
+                      />
+                      <Image
+                        src="/logo_white.svg"
+                        height={60}
+                        width={60}
+                        alt="gradient"
+                        className="animate-scroll2 absolute top-full left-1/2 -translate-x-1/2 block dark:hidden h-12 w-12"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                Discover
+                <div className="w-36 h-full border rounded-xl bg-foreground"></div>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              Hire
+              <div className="w-36 h-full border rounded-xl bg-foreground"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* U shape light to dark */}
+        {/* <Image
         src="/landing/dark_to_light_gradient_rotated.svg"
         height={1000}
         width={2000}
@@ -56,16 +159,24 @@ const LandingPage = () => {
         className="h-auto w-screen"
       /> */}
 
-      {/* U shape light to dark */}
-      <Image
-        src="/landing/light_to_dark_gradient.png"
-        height={1000}
-        width={2000}
-        alt="gradient"
-        className="h-auto w-screen"
-      />
-      <div className="h-screen bg-black"></div>
-    </div>
+        {/* U shape light to dark */}
+        <Image
+          src="/landing/light_to_dark_gradient.png"
+          height={1000}
+          width={2000}
+          alt="gradient"
+          className="block dark:hidden h-auto w-screen"
+        />
+        <Image
+          src="/landing/dark_to_light_gradient.svg"
+          height={1000}
+          width={2000}
+          alt="gradient"
+          className="hidden dark:block h-auto w-screen"
+        />
+        <div className="h-screen bg-black dark:bg-white"></div>
+      </div>
+    </SmoothScrollProvider>
   );
 };
 
---
+++ scrollnhire/components/ui/animated-underline.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export const AnimatedUnderline = ({
+  text = "Hover",
+  link = "/",
+}: {
+  text: string;
+  link: string;
+}) => {
+  return (
+    <div
+      // href={link}
+      className="text-muted-foreground hover:text-foreground transition-all duration-300 cursor-pointer relative after:absolute after:bg-foreground after:bottom-0 after:left-0 after:h-[0.5px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300"
+    >
+      <span className=" text-sm">{text}</span>
+    </div>
+  );
+};

cadde72140aec77a04d8461710c469ec98c77c19
Romildoescoding <romilrajrana1@gmail.com>
2026-05-05 14:13:27 +0530

    fix: useSearchParams wrapped in Suspense Boundary

--- scrollnhire/app/(auth)/signup/page.tsx
+++ scrollnhire/app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
 import { SignupForm } from "@/components/signup-form";
+import { Suspense } from "react";
 
 export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <SignupForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
